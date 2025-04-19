83bf3c890b6b02faa889ed9f9e814cc220075ba8
Michel Lenhart <80361711+michellenhart@users.noreply.github.com>
2025-04-19 10:29:33 -0300

    feat: first commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
 
 @Module({
   imports: [],
@@ -8,3 +8,4 @@ import { AppService } from './app.service';
   providers: [AppService],
 })
 export class AppModule {}
+
